f40609c8986f97e81455802e51d1c28a7894c936
indigo-spacefish <brandong85@gmail.com>
2015-06-09 19:40:20 -0700

    added comments with problem statement and challenge source

--- Hackerrank/sort_data_python.py
+++ Hackerrank/sort_data_python.py
@@ -1,3 +1,27 @@
+"""
+A Code Challenge from Hackerrank
+
+https://www.hackerrank.com/contests/pythonist/challenges/python-sort-sort
+
+From the contest page:
+
+
+"Problem Statement
+
+You are given data in a tabular format i.e. the data contains N rows and each row contains M space-separated elements.
+
+You can imagine the M items to be different attributes (like height, weight, energy, etc) and each of the N rows as an instance or a sample.
+
+Your task is to sort the table on the Kth attribute and print the final resulting table.
+
+[If two attributes are the same for different rows, print the row that appeared first in the input]
+
+Input Format First line contains N and M separated by a space.
+Next N lines contain M elements each.
+The last line contains K."
+
+"""
+
 test_case = "5 3\n10 2 5\n7 1 0\n9 9 9\n1 23 12\n6 5 9\n1"
 
 def sort_data(arg):
