949545d992311a3e42e3b5f8e691f2dd26aa2b44
Brandon Green <brandong85@gmail.com>
2016-01-12 13:34:51 -0800

    Create stock_profit.py

---
+++ stock_profit.py
@@ -0,0 +1,30 @@
+test_stock_prices = [310, 315, 275, 295, 260, 270, 290, 230, 255, 250]
+
+
+def compare_price(buy_price, sell_price):
+    profit = sell_price - buy_price
+    return profit
+
+
+def optimal_exchange(stock_prices):
+
+    possible_profits = []
+    iterator = 0
+    last_index = len(stock_prices)
+    result = {'Buy Price': 0, 'Sell Price': 0, 'Profit': 0}
+
+    for x in stock_prices:
+        # Shrink the loop to only compare stocks that come later chronologically
+        for i in range(iterator, last_index):
+            profit = compare_price(x, stock_prices[i])
+            possible_profits.append(profit)
+            if profit >= max(possible_profits):
+                result['Buy Price'] = x
+                result['Sell Price'] = stock_prices[i]
+                result['Profit'] = profit
+        iterator += 1
+    if result['Profit'] > 0:
+        print(result)
+
+
+optimal_exchange(test_stock_prices)
